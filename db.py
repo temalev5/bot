d4bd44fee938d60dadf6aae28570cd560454abed
temalev5 <49922049+temalev5@users.noreply.github.com>
2019-06-10 00:25:05 +0300

    Add files via upload

---
+++ db.py
@@ -0,0 +1,94 @@
+import sqlite3
+
+conn = sqlite3.connect('db.sqlite')
+
+def saveToBDset(chat_id,name,condition,value):
+    cursor = conn.cursor()
+    value_condition=condition
+    if (name=="рейтинг"):
+        name = "ex_rating"
+        condition = "ex_rating_condition"
+    if (name =="время"):
+        name = "ex_time"
+        condition = "ex_time_condition"
+    if (name =="год"):
+        name = "ex_year"
+        condition = "ex_year_condition"
+
+    cursor.execute('SELECT chats.' + name + ' FROM chats WHERE chat_id=?', (chat_id,))
+    result = cursor.fetchall()
+
+    if (result):
+        cursor.execute("UPDATE chats SET " + name + " = ?, "+ condition +" = ? WHERE chat_id=?",
+                       (value, value_condition, chat_id))
+    else:
+        cursor.execute('INSERT INTO chats(chat_id,' + name + ','+condition+') VALUES(?,?,?)',
+                       (chat_id, value,value_condition))
+
+    conn.commit()
+
+
+def saveToBD(chat_id,name,value,t):
+    cursor = conn.cursor()
+    if (name == "актера"): name = "ex_actors"
+    if (name == "жанр"): name = "ex_janre"
+    cursor.execute('SELECT chats.'+ name +' FROM chats WHERE chat_id=?', (chat_id,))
+    result = cursor.fetchall()
+    if (result):
+        if (result[0][0]):
+            ex_result_default = result[0][0]
+            if (t=="!исключить"):
+                ex_result = result[0][0].split(';')
+                ex_result.remove('')
+                for ex in ex_result:
+                    if (ex.lower() == value.lower()[:-1]):
+                        return False
+                setvalue = ex_result_default+value.lower()
+            elif (t=="!вернуть"):
+                setvalue = ex_result_default[0:ex_result_default.find(value)]+ex_result_default[ex_result_default.find(value)+len(value):]
+        else:
+            setvalue = value
+
+        cursor.execute("UPDATE chats SET "+name+" = ? WHERE chat_id=?",
+                       (setvalue,chat_id))
+    else:
+        cursor.execute('INSERT INTO chats(chat_id,'+name+') VALUES(?,?)',
+                       (chat_id,value.lower()))
+    conn.commit()
+
+def saveBD(chat):
+    cursor = conn.cursor()
+    for i in range(len(chat.man)):
+        movies = ""
+        cursor.execute('SELECT users.movies FROM users WHERE chat_id=? AND user_id=?', (chat.chat_id,chat.man[i].man_id))
+        result = cursor.fetchall()
+        for j in range(len(chat.man[i].movies)):
+            movies += chat.man[i].movies[j].title + "(" + str(chat.man[i].movies[j].id) + ")" + ";"
+        if (result):
+            cursor.execute("UPDATE users SET movies = ? WHERE chat_id=? AND user_id=?", (movies,chat.chat_id,chat.man[i].man_id,))
+        else:
+            cursor.execute('INSERT INTO users(user_id,chat_id,movies) VALUES(?,?,?)', (chat.man[i].man_id,chat.chat_id,movies))
+        conn.commit()
+
+
+def useDB(chat_id):
+    cursor = conn.cursor()
+    cursor.execute('SELECT '
+                   'chats.ex_rating, '
+                   'chats.ex_rating_condition,'
+                   'chats.ex_janre,'
+                   'chats.ex_year,'
+                   'chats.ex_year_condition,'
+                   'chats.ex_time,'
+                   'chats.ex_time_condition,'
+                   'chats.ex_actors,'
+                   'chats.ex_country'
+                   ' FROM chats WHERE chat_id=?', (chat_id,))
+    result = cursor.fetchall()
+    return result
+
+def useDBForMovies(user_id,chat_id):
+    cursor = conn.cursor()
+    cursor.execute('SELECT users.movies FROM users WHERE chat_id=? AND user_id=?', (chat_id,user_id) )
+    result = cursor.fetchall()
+    return result
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard,VkKeyboardColor
 from datetime import datetime
+from db import useDB,useDBForMovies,saveBD,saveToBD,saveToBDset
 from vk_api.utils import get_random_id
 from sm import NameToID,movieToText,getName,getNameByID
 import os
@@ -16,11 +17,68 @@ from lordfilm import SearchURLMovies
 #vk_session = vk_api.VkApi(login, password)
 #vk_session.auth()
 
-token = os.environ.get('BOT_TOKEN')
+#token = os.environ.get('BOT_TOKEN')
+token = '767809b3a110bf29b28438d837480302789fa7a87232ed8e8a2cd2925d18dedf753919bd340fb62d82f0d'
 vk_session = vk_api.VkApi(token=token)
 
 session_api = vk_session.get_api()
-longpoll = VkBotLongPoll(vk_session, '181453927')
+longpoll = VkBotLongPoll(vk_session, '181864086')
+
+
+
+def cheker(one,condition,two):
+
+    if ((one) and (two)):
+        if (condition == ">"):
+            if (one > two):
+                return "Нелегетимный"
+        if (condition == ">="):
+            if (one >= two):
+                return "Нелегетимный"
+        if (condition == "<"):
+            if (one < two):
+                return "Нелегетимный"
+        if (condition == "<="):
+            if (one <= two):
+                return "Нелегетимный"
+        if (condition == "="):
+            if (one != two):
+                return "Нелегетимный"
+
+def chekLegitimacy(movie,chat):
+    for i in range(len(chat.man)):
+        for j in range (len(chat.man[i].movies)):
+            if chat.man[i].movies[j].id == movie.id:
+                return "Повторяющийся фильм"
+
+    if (chat.ex_actors):
+        ex_actors = chat.ex_actors.split(';')
+        ex_actors.remove("")
+        for i in range(len(ex_actors)):
+            for j in range(len(movie.actors)):
+                if (ex_actors[i].lower()==movie.actors[j].name.lower()):
+                    return "Запрещенный актер " + movie.actors[j].name
+
+    if ((chat.ex_country) and (movie.countries)):
+        ex_country = chat.ex_country.split(';')
+        ex_country.remove("")
+        for i in range(len(ex_country)):
+            for j in range(len(movie.countries)):
+                if (ex_country[i].lower()==movie.countries[j].lower()):
+                    return "Запрещенная страна" + movie.countries[j]
+
+
+    if ((chat.ex_janre) and (movie.genres)):
+        ex_janre = chat.ex_janre.split(';')
+        ex_janre.remove("")
+        for i in range(len(ex_janre)):
+            for j in range(len(movie.genres)):
+                if (ex_janre[i].lower()==movie.genres[j].lower()):
+                    return "Запрещенный жанр " + movie.genres[j]
+
+    if (cheker(chat.ex_rating,chat.ex_rating_condition,movie.rating) == "Нелегетимный"): return "Не подходит по рейтингу "+ str(movie.rating)
+    if (cheker(chat.ex_time,chat.ex_time_condition,movie.runtime) == "Нелегетимный"): return "Не подходит по времени " + str(movie.runtime)
+    if (cheker(chat.ex_year, chat.ex_year_condition, movie.year) == "Нелегетимный"): return "Не подходит по году создания " + str(movie.year)
 
 
 def searchMan(rollMoive):
@@ -33,17 +91,17 @@ def searchMan(rollMoive):
         searchMan(rollMoive)
 
 
-def searchChat(rollMoive):
+def searchChat(rollMoive,id):
     only = []
     again_moviesroll = False
     try:
         for i in range(len(rollMoive)):
-            if rollMoive[i].chat_id == event.chat_id:
+            if rollMoive[i].chat_id == id:
                 only = rollMoive[i]
                 again_moviesroll = True
         return only,again_moviesroll
     except:
-        searchChat(rollMoive)
+        searchChat(rollMoive,id)
 
 def RemoveRollMovie(i,this, chat_id):
     time.sleep(1800)
@@ -58,25 +116,41 @@ def RemoveRollMovie(i,this, chat_id):
 
 
 def ChekAlreadyUse(man_id, chat_id):
-    try:
-        history = session_api.messages.getHistory(offset = 0, count = 25, user_id = man_id)
-        for i in range(25):
-            if history.get('items')[i].get('text')[0] == '$':
-                term = history.get('items')[i].get('text').find('\n')
-                message_chat_id = int(history.get('items')[i].get('text')[1:term])
-                if chat_id == message_chat_id:
-                    movies = history.get('items')[i].get('text').split('\n')
-                    movies.pop(0)
-                    break
-        movie = []
-        for i in range(len(movies)):
-            movie.append(getNameByID(movies[i][movies[i].find("(")+1:movies[i].find(")")]))
-        if len(movie) > 3:
-            movie = movie[0:3]
+    movies = useDBForMovies(man_id,chat_id)
+    movie = []
+    if (movies):
+        movies = movies[0][0]
+        movies = movies.split(';')
+        movies.remove("")
+        if (len(movies)<=3):
+            for i in range(len(movies)):
+                movie.append(getNameByID(movies[i][movies[i].find("(") + 1:movies[i].find(")")]))
+            if len(movie) > 3:
+                movie = movie[0:3]
+            return movie
+    else:
         return movie
-    except:
-        movies = []
-        return movies
+
+
+    #try:
+    #    history = session_api.messages.getHistory(offset = 0, count = 25, user_id = man_id)
+    #    for i in range(25):
+    #        if history.get('items')[i].get('text')[0] == '$':
+    #           term = history.get('items')[i].get('text').find('\n')
+    #            message_chat_id = int(history.get('items')[i].get('text')[1:term])
+    #            if chat_id == message_chat_id:
+    #                movies = history.get('items')[i].get('text').split('\n')
+    #                movies.pop(0)
+    #                break
+    #    movie = []
+    #    for i in range(len(movies)):
+    #        movie.append(getNameByID(movies[i][movies[i].find("(")+1:movies[i].find(")")]))
+    #    if len(movie) > 3:
+    #        movie = movie[0:3]
+    #    return movie
+    #except:
+    #    movies = []
+    #    return movies
 
 #def Typing(user_id):
     #boolTuping = True
@@ -93,6 +167,7 @@ class Man:
     def __init__(self, chat_id,man_id):
         self.chat_id = chat_id
         self.man_id = man_id
+        self.ex_movies = []
         self.movies = []
         self.mc = 0
         self.searchMovie = '0'
@@ -128,7 +203,27 @@ class ChatRoll:
         self.chat_id = event.chat_id
         self.man = []
         self.ex_rating = None
-        self.ex_rating_condition = None
+        self.ex_rating_condition = ">"
+        self.ex_janre = None
+        self.ex_year = None
+        self.ex_year_condition = ">"
+        self.ex_time = None
+        self.ex_time_condition = "<"
+        self.ex_actors = None
+        self.ex_country = None
+
+    def openBD(self):
+        result = useDB(self.chat_id)
+        if (result):
+            self.ex_rating = result[0][0]
+            if (result[0][1]): self.ex_rating_condition = result[0][1]
+            self.ex_janre = result[0][2]
+            self.ex_year = result[0][3]
+            if (result[0][4]): self.ex_year_condition = result[0][4]
+            self.ex_time = result[0][5]
+            if (result[0][6]): self.ex_time_condition = result[0][6]
+            self.ex_actors = result[0][7]
+            self.ex_country = result[0][8]
 
     def newMan(self, chat_id, man_id):
         self.man.append(Man(chat_id, man_id))
@@ -150,6 +245,7 @@ class ChatRoll:
             session_api.messages.send(peer_id=man[i].man_id,
                                       message=message,
                                       random_id=get_random_id())
+        saveBD(self)
 
     def roll(self):
         for i in range(len(self.man)):
@@ -231,13 +327,20 @@ while True:
                                                       '&#128172; [ !ролл ] - запуск выбора случайного фильма. &#10071; Работает только после '
                                                       'написания команды &#128172; [ !фильм ] &#128172; и при условии, что все, кто поставили '
                                                       '&#128172; [ + ] &#128172; скинули боту по 3 фильма &#10071;\n'
-                                                      '&#128172; [ !команды ] - скидывает список доступных команд в чат.\n'
+                                                      '&#128172; [ !команды ] - скидывает список доступных команд в чат\n'
+                                                      '&#128172; &#128081; [ !исключить <(актера,жанр)> <значение> ] - исключает фильмы с указанным актером'
+                                                      ' или жанром\n'
+                                                      '&#128172; &#128081; [ !вернуть <(актера,жанр)> <значение> ] - позволяет смотреть фильмы с указанным'
+                                                      ' актером, если он был когда либо исключен\n'
+                                                      '&#128172; &#128081; [ !установить <(рейтинг,год,жанр)> <Условие(>,<,>=,<=,=)> <значение> ] - позволяет '
+                                                      ' ограничить сбор фильмов по указанным парамерам \n'
                                                       '&#128172; [ !стоп ] - останавливает сбор фильмов\n'
+                                                      '&#10071; Команды со значком &#128081; доступны только администраторам чата\n'
                                                       '_______________________________________________________________',
                                               random_id=get_random_id())
                     break
 
-                only,again_moviesroll = searchChat(rollMoive)
+                only,again_moviesroll = searchChat(rollMoive,event.chat_id)
 
                 try:
                     man = only.getMan()
@@ -255,6 +358,7 @@ while True:
                     else:
                         rollMoive.append(ChatRoll(event))
                         thRM = rollMoive[len(rollMoive)-1]
+                        thRM.openBD()
                         threading.Thread(target=RemoveRollMovie, args=(0,thRM, event.chat_id),daemon=True).start()
                         print('Создал новый класс')
                         session_api.messages.send(chat_id=event.chat_id, message='Все, кто готов смотреть + в чат', random_id=get_random_id())
@@ -343,7 +447,7 @@ while True:
                                 #only.newMan(event.chat_id, event.obj.from_id)
                                 #ChekAlreadyUse(event.obj.from_id)
                             except:
-
+#
                                 userinfo = session_api.users.get(user_ids = event.obj.from_id)
                                 username = userinfo[0].get('first_name')
 
@@ -353,7 +457,7 @@ while True:
                                                             'Либо просто напиши мне первый'
 
                                 session_api.messages.send(chat_id=event.chat_id, message= message ,
-                                                    random_id=get_random_id())
+                                                  random_id=get_random_id())
                                 break
 
                 elif event.obj.text.lower() == '!ролл':
@@ -387,7 +491,59 @@ while True:
                         session_api.messages.send(chat_id=event.chat_id,
                                                   message='Сбор фильмов остановлен',
                                                   random_id=get_random_id())
-                    #t.join()
+
+                elif (event.obj.text.lower()[0:10] == '!исключить' or event.obj.text.lower()[0:8]=='!вернуть'):
+                    t = event.obj.text[:event.obj.text.find(' ')]
+                    items = session_api.messages.getConversationMembers(peer_id = 2000000000+event.chat_id).get('items')
+                    for item in items:
+                        if (item.get('member_id') == event.obj.from_id):
+                            if (item.get('is_admin')):
+                                name = event.obj.text[event.obj.text.find(' ') + 1:]
+                                name = name[:name.find(' ')].lower()
+                                if (name == "актера") or (name == "жанр"):
+                                    value = event.obj.text[event.obj.text.find(' ') + 1:]
+                                    value = value[value.find(' ') + 1:]+";"
+                                    validate = saveToBD(event.chat_id,name,value.lower(),t)
+                    if validate==False:
+                        break
+                    message = ""
+                    if (name == "актера"): name = "актер"
+                    if (t=="!исключить"):
+                        message = "&#128204; Исключен " + name + ' &#128253; ' + value[:-1] + ' &#128253;'
+                        a = 10
+                    elif (t=="!вернуть"):
+                        message = "&#128204; " + name + ' &#128253; ' + value[:-1] + " &#128253; теперь доступен &#9989;"
+
+                    session_api.messages.send(chat_id=event.chat_id,
+                                              message=message,
+                                              random_id=get_random_id())
+
+
+
+                elif (event.obj.text.lower()[0:11] == '!установить'):
+                    items = session_api.messages.getConversationMembers(peer_id=2000000000 + event.chat_id).get('items')
+                    for item in items:
+                        if (item.get('member_id') == event.obj.from_id):
+                            if (item.get('is_admin')):
+                                name = event.obj.text[event.obj.text.find(' ') + 1:]
+                                name = name[:name.find(' ')].lower()
+                                if (name == "рейтинг") or (name == "время") or (name == "год"):
+                                    condition = event.obj.text[event.obj.text.find(' ') + 1:]
+                                    condition = condition[condition.find(' ') + 1:]
+                                    condition = condition[:condition.find(' ')]
+                                    if (condition == ">") or (condition == "<") or (condition == ">=") or (condition == "<=") or (condition == "="):
+                                        value = event.obj.text[event.obj.text.find(' ') + 1:]
+                                        value = value[value.find(' ') + 1:]
+                                        value = value[value.find(' ')+1:]
+                                        saveToBDset(event.chat_id,name,condition,value)
+                    message = "&#128204; Установлен параметр &#128253; "+ name + " " + "фильма &#128253; " + condition + " &#11088;" + value + " &#11088;"
+                    session_api.messages.send(chat_id=event.chat_id,
+                                              message=message,
+                                              random_id=get_random_id())
+                    a=10
+
+
+
 
             if event.from_user:
                 man = []
@@ -440,6 +596,18 @@ while True:
                             #t.join()
                         elif event.obj.text == '+':
                             only.countFindFilm = 0
+
+                            seconly = searchChat(rollMoive, only.chat_id)[0]
+
+                            legetimacy = chekLegitimacy(only.targetMovie,seconly)
+
+                            if (legetimacy):
+                                message = "Этот фильм нелегитимен! Причина: "+ legetimacy
+                                session_api.messages.send(peer_id=event.obj.from_id,
+                                                          message=message,
+                                                          random_id=get_random_id())
+                                break
+
                             only.SetMovies()
 
                             #keybord = VkKeyboard(one_time=True)
